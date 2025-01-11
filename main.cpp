7e5e2162f1f14f9e32fc10f4708e93010b55b278
Mayank <166874794+Mayank7371@users.noreply.github.com>
2025-01-11 21:32:32 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
     #include<bits/stdc++.h>
+    #include<unistd.h>
     using namespace std;
-int main() {
-    cout << "Hello, World!" << endl;
+
+    void pipleine(const char* process1,const char* process2){
+        int fd[2];
+        pipe(fd);
+        if(fork()!= 0){ //parent process
+        close(fd[0]);
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[1]);
+
+        execlp("/bin/cat","cat","main.cpp",nullptr);
+        cerr<<"failed to execute "<<process1<<endl;
+        }else{
+        close(fd[1]);
+        dup2(fd[0], STDOUT_FILENO);
+        close(fd[0]);
+        execlp("user/bin/grep","grep","hello",nullptr);
+        cerr<<"failed to execute "<<process2<<endl;
+        }
+
+        }
+    
+    int main() {
+    pipleine("cat main.cpp", "grep hello");
     return 0;
 }
