3d145bc70f2cdefd114b20ed24ff38314548331b
Mayank <166874794+Mayank7371@users.noreply.github.com>
2025-01-19 08:44:31 +0530

    correct the code

--- main.cpp
+++ main.cpp
@@ -7,18 +7,18 @@
         pipe(fd);
         int id = fork();
         if(id != 0){ //parent process 
-        close(fd[0]);
-        dup2(fd[1], STDOUT_FILENO);
-        close(fd[1]);
+        close(fd[0]); // close the read end of the pipe
+        dup2(fd[1], STDOUT_FILENO); // redirect std output to the write end of the pipe
+        close(fd[1]); //close the write end of the pipe 
 
-        execlp("/bin/cat","cat","main.cpp",nullptr);
-        cerr<<"failed to execute "<<process1<<endl;
+        execlp("/usr/bin/cat","cat","main.cpp",nullptr);
+        cerr  <<  "failed to execute "  <<  process1  <<  endl;
         }else{
-        close(fd[1]);
-        dup2(fd[0], STDOUT_FILENO);
-        close(fd[0]);
-        execlp("user/bin/grep","grep","hello",nullptr);
-        cerr<<"failed to execute "<<process2<<endl;
+        close(fd[1]); // Close the write end of the pipe
+        dup2(fd[0], STDIN_FILENO); // Redirect stdin to the read end of the pipe
+        close(fd[0]); // Close the read end of the pipe
+        execlp("/usr/bin/grep","grep","hello",nullptr);
+        cerr  <<"failed to execute "  <<  process2  <<endl;
         }
 
         }
